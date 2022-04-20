32d2d36b5663d458e6e3f1d48063992cc7b01b9e
Marco Antonio Alonso <marcogd022@gmail.com>
2022-04-20 18:42:26 -0300

    Create todolist with PySimpleGUI.py

---
+++ todolist with PySimpleGUI.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+def criar_janela_inicial():
+    sg.theme('DarkBlue4')
+    linha = [
+        [sg.Checkbox(''), sg.Input('')]
+    ]
+    layout = [
+        [sg.Frame('Tarefas', layout=linha, key='container')],
+        [sg.Button('Nova Tarefa'), sg.Button('Resetar')]
+    ]
+
+    return sg.Window('Todo List', layout=layout, finalize=True)
+
+#Criar a janela
+janela = criar_janela_inicial()
+
+#Criar as regras dessa janela
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Nova Tarefa':
+        janela.extend_layout(janela['container'],
+        [[sg.Checkbox(''), sg.Input('')]])  # layout precisa estar fechado em colchetes
+    elif event == 'Resetar':
+        janela.close()
+        janela = criar_janela_inicial()
