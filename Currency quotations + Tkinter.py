33ea38f9184c7299af7805ec58f2fbfdcaaea843
Marco Antonio Alonso <marcogd022@gmail.com>
2022-04-24 15:22:46 -0300

    Update Currency quotations + Tkinter.py

--- Currency quotations + Tkinter.py
+++ Currency quotations + Tkinter.py
@@ -1,21 +1,23 @@
 import requests
 from tkinter import *
 
+
 def pegar_cotacoes():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
     requisicao_dic = requisicao.json()
 
-    cotacao_dolar = requisicao_dic['USD']['bid']
-    cotacao_euro = requisicao_dic['EUR']['bid']
-    cotacao_btc = requisicao_dic['BTC']['bid']
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
 
     texto = f'''
     Dólar: {cotacao_dolar}
     Euro: {cotacao_euro}
     BTC: {cotacao_btc}'''
-    
+
     texto_cotacoes['text'] = texto
+
 
 janela = Tk()
 janela.title("Cotaçoes")
